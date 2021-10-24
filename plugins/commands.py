1973617415e756557945189b0f94ccb87ab8341c
Osho Raj <65038390+Osho28Raj@users.noreply.github.com>
2021-10-24 16:43:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -97,3 +97,184 @@ async def show_help(client, message):
         reply_markup=reply_markup,
         disable_web_page_preview=True
         )
+
+@Client.on_message(filters.command(['restart', 'update', f"restart@{Config.BOT_USERNAME}", f"update@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
+async def update_handler(client, message):
+    if Config.HEROKU_APP:
+        k = await message.reply("Heroku APP found, Restarting app to update.")
+        if Config.DATABASE_URI:
+            msg = {"msg_id":k.message_id, "chat_id":k.chat.id}
+            if not await db.is_saved("RESTART"):
+                db.add_config("RESTART", msg)
+            else:
+                await db.edit_config("RESTART", msg)
+            await sync_to_db()
+    else:
+        k = await message.reply("No Heroku APP found, Trying to restart.")
+        if Config.DATABASE_URI:
+            msg = {"msg_id":k.message_id, "chat_id":k.chat.id}
+            if not await db.is_saved("RESTART"):
+                db.add_config("RESTART", msg)
+            else:
+                await db.edit_config("RESTART", msg)
+    try:
+        await message.delete()
+    except:
+        pass
+    await update()
+
+@Client.on_message(filters.command(['logs', f"logs@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
+async def get_logs(client, message):
+    m=await message.reply("Checking logs..")
+    if os.path.exists("botlog.txt"):
+        await message.reply_document('botlog.txt', caption="Bot Logs")
+        await m.delete()
+        await delete_messages([message])
+    else:
+        k = await m.edit("No log files found.")
+        await delete_messages([message, k])
+
+@Client.on_message(filters.command(['env', f"env@{Config.BOT_USERNAME}", "config", f"config@{Config.BOT_USERNAME}"]) & sudo_filter & chat_filter)
+async def set_heroku_var(client, message):
+    with suppress(MessageIdInvalid, MessageNotModified):
+        m = await message.reply("Checking config vars..")
+        if " " in message.text:
+            cmd, env = message.text.split(" ", 1)
+            if "=" in env:
+                var, value = env.split("=", 1)
+            else:
+                if env == "STARTUP_STREAM":
+                    env_ = "STREAM_URL"
+                elif env == "QUALITY":
+                    env_ = "CUSTOM_QUALITY" 
+                else:
+                    env_ = env
+                ENV_VARS = ["ADMINS", "SUDO", "CHAT", "LOG_GROUP", "STREAM_URL", "SHUFFLE", "ADMIN_ONLY", "REPLY_MESSAGE", 
+                        "EDIT_TITLE", "RECORDING_DUMP", "RECORDING_TITLE", "IS_VIDEO", "IS_LOOP", "DELAY", "PORTRAIT", 
+                        "IS_VIDEO_RECORD", "PTN", "CUSTOM_QUALITY"]
+                if env_ in ENV_VARS:
+                    await m.edit(f"Current Value for `{env}`  is `{getattr(Config, env_)}`")
+                    await delete_messages([message])
+                    return
+                else:
+                    await m.edit("This is an invalid env value. Read help on env to know about available env vars.")
+                    await delete_messages([message, m])
+                    return     
+            
+        else:
+            await m.edit("You haven't provided any value for env, you should follow the correct format.\nExample: <code>/env CHAT=-1020202020202</code> to change or set CHAT var.\n<code>/env REPLY_MESSAGE= <code>To delete REPLY_MESSAGE.")
+            await delete_messages([message, m])
+            return
+
+        if Config.DATABASE_URI and var in ["STARTUP_STREAM", "CHAT", "LOG_GROUP", "REPLY_MESSAGE", "DELAY", "RECORDING_DUMP", "QUALITY"]:      
+            await m.edit("Mongo DB Found, Setting up config vars...")
+            await asyncio.sleep(2)  
+            if not value:
+                await m.edit(f"No value for env specified. Trying to delete env {var}.")
+                await asyncio.sleep(2)
+                if var in ["STARTUP_STREAM", "CHAT", "DELAY"]:
+                    await m.edit("This is a mandatory var and cannot be deleted.")
+                    await delete_messages([message, m]) 
+                    return
+                await edit_config(var, False)
+                await m.edit(f"Sucessfully deleted {var}")
+                await delete_messages([message, m])           
+                return
+            else:
+                if var in ["CHAT", "LOG_GROUP", "RECORDING_DUMP", "QUALITY"]:
+                    try:
+                        value=int(value)
+                    except:
+                        if var == "QUALITY":
+                            if not value.lower() in ["low", "medium", "high"]:
+                                await m.edit("You should specify a value between 10 - 100.")
+                                await delete_messages([message, m])
+                                return
+                            else:
+                                value = value.lower()
+                                if value == "high":
+                                    value = 100
+                                elif value == "medium":
+                                    value = 66.9
+                                elif value == "low":
+                                    value = 50
+                        else:
+                            await m.edit("You should give me a chat id . It should be an interger.")
+                            await delete_messages([message, m])
+                            return
+                    if var == "CHAT":
+                        await leave_call()
+                        Config.ADMIN_CACHE=False
+                        if Config.IS_RECORDING:
+                            await stop_recording()
+                        await cancel_all_schedules()
+                        Config.CHAT=int(value)
+                        await restart()
+                    await edit_config(var, int(value))
+                    if var == "QUALITY":
+                        if Config.CALL_STATUS:
+                            data=Config.DATA.get('FILE_DATA')
+                            if not data \
+                                or data.get('dur', 0) == 0:
+                                await restart_playout()
+                                return
+                            k, reply = await seek_file(0)
+                            if k == False:
+                                await restart_playout()
+                    await m.edit(f"Succesfully changed {var} to {value}")
+                    await delete_messages([message, m])
+                    return
+                else:
+                    if var == "STARTUP_STREAM":
+                        Config.STREAM_SETUP=False
+                    await edit_config(var, value)
+                    await m.edit(f"Succesfully changed {var} to {value}")
+                    await delete_messages([message, m])
+                    await restart_playout()
+                    return
+        else:
+            if not Config.HEROKU_APP:
+                buttons = [[InlineKeyboardButton('Heroku API_KEY', url='https://dashboard.heroku.com/account/applications/authorizations/new'), InlineKeyboardButton('🗑 Close', callback_data='close'),]]
+                await m.edit(
+                    text="No heroku app found, this command needs the following heroku vars to be set.\n\n1. <code>HEROKU_API_KEY</code>: Your heroku account api key.\n2. <code>HEROKU_APP_NAME</code>: Your heroku app name.", 
+                    reply_markup=InlineKeyboardMarkup(buttons)) 
+                await delete_messages([message])
+                return     
+            config = Config.HEROKU_APP.config()
+            if not value:
+                await m.edit(f"No value for env specified. Trying to delete env {var}.")
+                await asyncio.sleep(2)
+                if var in ["STARTUP_STREAM", "CHAT", "DELAY", "API_ID", "API_HASH", "BOT_TOKEN", "SESSION_STRING", "ADMINS"]:
+                    await m.edit("These are mandatory vars and cannot be deleted.")
+                    await delete_messages([message, m])
+                    return
+                if var in config:
+                    await m.edit(f"Sucessfully deleted {var}")
+                    await asyncio.sleep(2)
+                    await m.edit("Now restarting the app to make changes.")
+                    if Config.DATABASE_URI:
+                        msg = {"msg_id":m.message_id, "chat_id":m.chat.id}
+                        if not await db.is_saved("RESTART"):
+                            db.add_config("RESTART", msg)
+                        else:
+                            await db.edit_config("RESTART", msg)
+                    del config[var]                
+                    config[var] = None               
+                else:
+                    k = await m.edit(f"No env named {var} found. Nothing was changed.")
+                    await delete_messages([message, k])
+                return
+            if var in config:
+                await m.edit(f"Variable already found. Now edited to {value}")
+            else:
+                await m.edit(f"Variable not found, Now setting as new var.")
+            await asyncio.sleep(2)
+            await m.edit(f"Succesfully set {var} with value {value}, Now Restarting to take effect of changes...")
+            if Config.DATABASE_URI:
+                msg = {"msg_id":m.message_id, "chat_id":m.chat.id}
+                if not await db.is_saved("RESTART"):
+                    db.add_config("RESTART", msg)
+                else:
+                    await db.edit_config("RESTART", msg)
+            config[var] = str(value)
+
